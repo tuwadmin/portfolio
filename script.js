601a59982f244542df54e5f27823f31729dc390b
Samia Lemfadli <50528819+tuwadmin@users.noreply.github.com>
2025-08-07 22:23:16 +0000

    added hero, testimonials, and more styling

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    img: 'https://www.course-api.com/images/people/person-1.jpeg',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    img: 'https://www.course-api.com/images/people/person-2.jpeg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    img: 'https://www.course-api.com/images/people/person-4.jpeg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
+  },
+];
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const clouds = document.querySelector('.clouds');
-const gameOver = document.querySelector('.game_over');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
-const jump = () => {
-  mario.classList.add('jump');
+// set starting item
+let currentItem = 0;
 
-  setTimeout(() => {
-    mario.classList.remove('jump');
-  }, 500);
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
 }
-
-
-const loop = setInterval(() => {
-
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-  const cloudsPosition = +window.getComputedStyle(clouds).left.replace('px', '');
-
-  
-  if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
-    pipe.style.animation = 'none';
-    pipe.style.left = `${pipePosition}px`;
-
-    mario.style.animation = 'none';
-    mario.style.bottom = `${marioPosition}px`;
-
-    clouds.style.animation = 'none';
-    clouds.style.left = `${cloudsPosition}px`;
-
-    mario.src = "images/game-over.png";
-    mario.style.width = '75px';
-    mario.style.marginLeft = '50px';
-
-    gameOver.textContent = "Game over";
-
-    clearInterval(loop);
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
   }
-
-}, 15)
-
-document.addEventListener('keydown', jump);
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
